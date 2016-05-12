23bda62d4b32e24dc01502d5c81451d68ea44e8e
Aitux <simon2310mike@gmail.com>
2016-05-12 22:02:37 +0200

    LastMinuteChanges

--- src/BotOMatic.java
+++ src/BotOMatic.java
@@ -1,44 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.security.auth.login.LoginException;
-
-import net.dv8tion.jda.MessageBuilder;
 import net.dv8tion.jda.entities.User;
 import net.dv8tion.jda.entities.impl.JDAImpl;
 
 public class BotOMatic {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		JDAImpl jda = new JDAImpl(true);
-		MessageBuilder mb = new MessageBuilder();
 		List<User> connected = new ArrayList<>();
 		try {
 			jda.login("MTc5OTkwMDg2MTQwNjkwNDMy.ChZmkA.EBzlH81byYdUtMYQeLdMYLT03o8");
 		} catch (LoginException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		connected = jda.getUsers();
-		System.out.println(connected.toString());
-		/*	jda.addListener(new ListenerAdapter() {
-
-			@Override
-			public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
-				// TODO Auto-generated method stub
-				super.onPrivateMessageReceived(event);
-
-				mb.appendString("HelloWorld");
-				mb.build();
-			}
-
-		});
-
-	}*/
+		}	
+		connected = jda.getUsersByName("Aitux");
+		System.out.println(connected.size());
 	}
 }
