1ac03290e54e1bb7d6136a6f8643efce0cfeaaa6
Aitux <simon2310mike@gmail.com>
2016-05-12 21:13:26 +0200

    IL EST VIVANT

--- src/BotOMatic.java
+++ src/BotOMatic.java
@@ -1,9 +1,22 @@
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.JDABuilder;
 
 public class BotOMatic {
 
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
-		
+		JDABuilder jda = new JDABuilder();
+		jda.setBotToken("MTc5OTkwMDg2MTQwNjkwNDMy.ChZmkA.EBzlH81byYdUtMYQeLdMYLT03o8");
+		try {
+			jda.buildAsync();
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
