91ed62627436eb59ac5bbe463c6f38d6844f9978
norbana <antoine.critelli@gmx.fr>
2016-05-12 21:16:09 +0200

    rien

--- src/BotOMatic.java
+++ src/BotOMatic.java
@@ -1,13 +1,8 @@
-
 public class BotOMatic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 
-		
-=======
->>>>>>> 36afeabb59788c816d6cc4fe9a873f48eae31423
 		
 	}
 
