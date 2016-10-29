6979eb5f5878301be010dd6affe33ef1b9445668
Aitux <simon2310mike@gmail.com>
2016-10-29 10:54:41 +0200

    Big Update

--- src/tps/BotOMatic.java
+++ src/tps/BotOMatic.java
@@ -1,22 +1,77 @@
 package tps;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.JDA;
 import net.dv8tion.jda.JDABuilder;
+import net.dv8tion.jda.events.ShutdownEvent;
+import net.dv8tion.jda.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.managers.AccountManager;
+import net.dv8tion.jda.utils.AvatarUtil;
+
 
 public class BotOMatic {
 	public static void main(String[] args) {
-		JDA jda = null;
-		
+
+		Properties prop = new Properties();
+		File f = new File("C:\\Users\\simon\\Documents\\properties.txt");
 		try {
-			jda = new JDABuilder().setBotToken("MTc5OTkwMDg2MTQwNjkwNDMy.ChZmkA.EBzlH81byYdUtMYQeLdMYLT03o8").buildBlocking();
-			jda.addEventListener(new DiscordMessageListener());
+			FileInputStream fl = new FileInputStream(f);
+			prop.load(fl);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			System.exit(0);
+		}	
+		try {
+			final JDA jda = new JDABuilder().setBotToken(prop.getProperty("token")).setBulkDeleteSplittingEnabled(false)
+					.buildBlocking();
+			jda.addEventListener(new DiscordMessageListener() {
+				public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
+					super.onGuildMessageReceived(e);
+					if (e.getMessage().getContent().contains("!shutdown")) {
+						jda.shutdown();
+					}
+					if (e.getMessage().getContent().contains("!update")) {
+						if (e.getAuthor().getId().equals(prop.getProperty("sudoId"))) {
+							AccountManager ac = jda.getAccountManager();
+							if (e.getMessage().getContent().contains("Data")) {
+								File img = new File(prop.getProperty("imgPath"));
+								ac.setGame("Funny Tricks");
+								ac.setUsername("Administrateur");
+								try {
+									ac.setAvatar(AvatarUtil.getAvatar(img));
+									System.out.println("Image updated");
+								} catch (UnsupportedEncodingException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+								ac.update();
+								System.out.println("Data updated");
+							} else if (e.getMessage().getContent().contains("stream")) {
+								ac.setStreaming("I'm Working on... me", "https://www.twitch.tv/aitux");
+							} else if (e.getMessage().getContent().contains("reset")) {
+								ac.setStreaming(null, null);
+							}
+						}
+					}
+				}
+
+				@Override
+				public void onShutdown(ShutdownEvent event) {
+					super.onShutdown(event);
+
+				}
+			});
 		} catch (LoginException | IllegalArgumentException | InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+			System.exit(0);
+		}
 	}
 
-
 }
-
--- src/tps/DiscordMessageListener.java
+++ src/tps/DiscordMessageListener.java
@@ -1,48 +1,96 @@
 package tps;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.dv8tion.jda.entities.Guild;
+import net.dv8tion.jda.entities.VoiceChannel;
+import net.dv8tion.jda.entities.impl.VoiceChannelImpl;
 import net.dv8tion.jda.events.ShutdownEvent;
 import net.dv8tion.jda.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.hooks.ListenerAdapter;
+import net.dv8tion.jda.managers.GuildManager;
 
-public class DiscordMessageListener extends ListenerAdapter{
+public class DiscordMessageListener extends ListenerAdapter {
 
-	
+	List<VoiceChannel> voiceList;
+
 	@Override
 	public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
-		// TODO Auto-generated method stub
+
 		super.onGuildMessageReceived(e);
+		boolean flag = false;
 		DiscordStream stream = new DiscordStream(e.getChannel());
-		if(e.getMessage().getContent().equals("!insulteMoi")){
-			stream.printToDiscord(e.getAuthor().getUsername() + " il semblerait que tu aimes te faire insulter ma salope !");
+		setChannel(e, stream);
+		if (!e.getAuthor().isBot()) {
+			if (e.getMessage().getContent().contains("!moveTo")) {
+				String channel = "";
+				channel = e.getMessage().getContent().split("\\s+")[1];
+				// stream.printToDiscord(e.getMessage().getContent());
+				if (!channel.equals("")) {
+					for (int i = 0; i < voiceList.size(); i++) {
+						if (channel.equals(voiceList.get(i).getName())) {
+							channel = voiceList.get(i).getId();
+							flag = true;
+						}
+					}
+					if (!flag)
+						channel = "";
+				} else {
+					stream.printToDiscord(
+							"Je n'ai pas compris, veuillez vous assurer de suivre la syntaxe suivante: ```!moveTo channel``` :exclamation: Avec 1 seul espace.");
+					System.out.println("Channel = [" + channel + "]");
+				}
+				GuildManager gm = new GuildManager(e.getGuild());
+				Guild g = e.getGuild();
+				if (e.getGuild().getVoiceStatusOfUser(e.getAuthor()).inVoiceChannel()) {
+					if (!channel.equals("")) {
+						gm.moveVoiceUser(e.getAuthor(), new VoiceChannelImpl(channel, e.getGuild()));
+					} else
+						stream.printToDiscord(
+								"Je n'ai pas compris, veuillez vous assurer de suivre la syntaxe suivante: ```!moveTo channel``` :name_badge: Avec 1 seul espace.");
+				} else {
+					stream.printToDiscord(
+							"Vous devez être connecté à un Voice Channel pour pouvoir être move. :expressionless:");
+				}
+			}
+			
+			if(e.getMessage().getContent().contains("!shutdown")){
+				
+			}
 		}
-	/*	if(e.getAuthor().getUsername().equals("norbana")){
-			e.getMessage().deleteMessage();
-			stream.printToDiscord(e.getAuthor().getUsername() + " __vous n'avez pas le droit à la parole ici !__");
-			stream.setPrivate(e.getAuthor().getPrivateChannel());
-			stream.printPrivate("Veuillez ne pas troubler l'ordre sur le serveur sous peine de banissement !");
-		}*/
-		
+
+		/*
+		 * if(e.getAuthor().getUsername().equals("norbana")){
+		 * e.getMessage().deleteMessage();
+		 * stream.printToDiscord(e.getAuthor().getUsername() +
+		 * " __vous n'avez pas le droit à la parole ici !__");
+		 * stream.setPrivate(e.getAuthor().getPrivateChannel());
+		 * stream.printPrivate(
+		 * "Veuillez ne pas troubler l'ordre sur le serveur sous peine de banissement !"
+		 * ); }
+		 */
+
 	}
 
 
-
-	@Override
-	public void onShutdown(ShutdownEvent event) {
-		// TODO Auto-generated method stub
-		super.onShutdown(event);
-	}
 
 	@Override
 	public void onPrivateMessageReceived(PrivateMessageReceivedEvent e) {
 		// TODO Auto-generated method stub
 		super.onPrivateMessageReceived(e);
-
 		DiscordStream stream = new DiscordStream(e.getChannel(), true);
-		if(!e.getAuthor().isBot()){
-			if(e.getMessage().getContent().equals("!coquin")) stream.printPrivate("https://www.instagram.com/p/BFXjc5QIV9Z/?taken-by=nickygile");
-			//stream.printPrivate("Je n'aime pas que l'on me parle en privé ca me gène :S");
+		if (!e.getAuthor().isBot()) {
+			if (e.getMessage().getContent().equals("!coquin"))
+				stream.printPrivate(":underage: https://www.instagram.com/highestheaven/ :underage:");
+			stream.printPrivate("Je n'aime pas que l'on me parle en privé ca me gène :flushed:");
 		}
+	}
+
+	private void setChannel(GuildMessageReceivedEvent e, DiscordStream stream) {
+		voiceList = e.getGuild().getVoiceChannels();
 	}
 
 }
